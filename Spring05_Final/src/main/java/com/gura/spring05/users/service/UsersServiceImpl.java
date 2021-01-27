a82df9ee4306360066ba2ec32e460ba975bc8372
kimhongju <kinhoz9321@gmail.com>
2021-01-27 11:18:16 +0900

    userscontroller 다이어트 /users/loginform.do 요청 처리함

--- Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersServiceImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/users/service/UsersServiceImpl.java
@@ -1,7 +1,11 @@
 package com.gura.spring05.users.service;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.gura.spring05.users.dao.UsersDao;
 import com.gura.spring05.users.dto.UsersDto;
@@ -23,4 +27,41 @@ public class UsersServiceImpl implements UsersService{
 		return dao.isExist(inputId);
 	}
 
+	@Override
+	public void loginformLogic(HttpServletRequest request, ModelAndView mView) {
+		// GET 방식 파라미터 url 이라는 이름으로 전달되는 값이 있는지 읽어와보기
+		String url=request.getParameter("url");
+		//만일 넘어오는 값이 없다면 
+		if(url==null){
+			//로그인 후에 home.do 요청이 되도록 절대 경로를 구성한다.
+			String cPath=request.getContextPath();
+			url=cPath+"/home.do";
+		}
+		//쿠키에 저장된 아이디와 비밀번호를 담을 변수
+		String savedId="";
+		String savedPwd="";
+		//쿠키에 저장된 값을 위의 변수에 저장하는 코드를 작성해 보세요.
+		Cookie[] cooks=request.getCookies();
+		if(cooks!=null){
+			//반복문 돌면서 쿠키객체를 하나씩 참조해서 
+			for(Cookie tmp: cooks){
+				//저장된 키값을 읽어온다.
+				String key=tmp.getName();
+				//만일 키값이 savedId 라면 
+				if(key.equals("savedId")){
+					//쿠키 value 값을 savedId 라는 지역변수에 저장
+					savedId=tmp.getValue();
+				}
+				if(key.equals("savedPwd")){
+					savedPwd=tmp.getValue();
+				}
+			}
+		}		
+		//view page 에서 필요한 데이터를 담는다.
+		mView.addObject("url", url);
+		mView.addObject("savedId", savedId);
+		mView.addObject("savedPwd", savedPwd);
+	}
+	
+	
 }
